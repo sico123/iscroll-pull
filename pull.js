6ffad6a5cc2b0452af447cd1432da3229d06af17
shishunan <shishunan123@gmail.com>
2017-12-12 00:27:36 +0800

    增加事例

--- pull.js
+++ pull.js
@@ -6,6 +6,35 @@ function loaded(){
     var myScroll = new IScroll('.wrapper',{
         scrollbars: false,
         snap: 'li',
-        mouseWheel: true
+        mouseWheel: true,
+        probeType: 3
     });
+    window.scroller = myScroll;
+    bindEvent();
+}
+
+function bindEvent(){
+    window.scroller.on('scroll', function(event){
+        let Y = this.y;
+        console.log(Y)
+        let pullDownEl = document.querySelector('.pull-down'),
+            pullUpEl = document.querySelector('.pull-up'),
+            pullDownShow = pullDownEl.classList.contains('show'),
+            pullUpShow = pullUpEl.classList.contains('show');
+        if(Y > 40){
+            !pullDownShow && pullDownEl.classList.add('show');
+            pullDownEl.innerHTML = "松开刷新";
+            this.minScrollY = 0;
+            return ;
+        } else if(Y < 40 && Y > 0){
+            pullDownEl && pullDownEl.classList.remove('show');
+            pullDownEl.innerHTML = "继续下拉刷新";
+            this.minScrollY = 40;
+        }
+    })
+    window.scroller.on('scrollEnd', function(event){
+        console.log('触发　scrollEnd');
+        console.log(event);
+    });
+    
 }
